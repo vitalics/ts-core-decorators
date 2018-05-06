bf6f86be02fc3a5eb6260191d7867a334a183885
vitalicset <vitalicset@yandexru>
2018-05-06 22:21:28 +0300

    add description for decorator

--- src/decorators/common/autowired.decorator.ts
+++ src/decorators/common/autowired.decorator.ts
@@ -1,10 +1,15 @@
 import 'reflect-metadata';
 
+
+/**
+ * @param args args for constructor
+ * @description auto creating new instance with args
+ */
 export function autowired(...args: any[]) {
     return function (target: any, propertyKey: string) {
         const type = Reflect.getMetadata('design:type', target, propertyKey);
         const instance = Reflect.construct(type, args || undefined);
-        Reflect.defineMetadata(`${target}_aurowired`, { autowired: instance }, target);
+        Reflect.defineMetadata(`${target}_autowired`, { autowired: instance }, target);
         Reflect.defineProperty(target, propertyKey, {
             value: instance
         });
